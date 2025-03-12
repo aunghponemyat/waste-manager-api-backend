c67649788923a29d7344845edf1c30b3c6539bf9
toe-recyglo <86629533+toe-recyglo@users.noreply.github.com>
2025-03-12 15:40:09 +0700

    Update aws.js (recyglo-b2b to waste-b2b)

--- config/aws.js
+++ config/aws.js
@@ -10,7 +10,7 @@ AWS.config.apiVersions = {
   s3: '2006-03-01',
 };
 
-const s3bucket = new AWS.S3({ params: { Bucket: 'recyglo-b2b' } });
+const s3bucket = new AWS.S3({ params: { Bucket: 'waste-b2b' } });
 
 module.exports = {
   s3: s3bucket,
